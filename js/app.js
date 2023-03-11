8a083b3c90e4f6903a6b29d47e65c53e246bd962
Thoriq Satriya <thoriq@accelbyte.net>
2023-03-11 08:48:02 +0700

    add query parameter

--- js/app.js
+++ js/app.js
@@ -20,9 +20,10 @@ const audio = (() => {
     };
 })();
 
+let eventTime = '2023-04-29 11:00:00'
+
 const timer = () => {
-    let tanggal = document.getElementById('timestamp').getAttribute('data-waktu').toString();
-    let countDownDate = new Date(tanggal).getTime();
+    let countDownDate = new Date(eventTime).getTime();
     let time = null;
 
     time = setInterval(() => {
@@ -62,78 +63,26 @@ const play = (btn) => {
     }
 };
 
-const resetForm = () => {
-    document.getElementById('kirim').style.display = 'block';
-    document.getElementById('hadiran').style.display = 'block';
-    document.getElementById('labelhadir').style.display = 'block';
-    document.getElementById('batal').style.display = 'none';
-    document.getElementById('kirimbalasan').style.display = 'none';
-    document.getElementById('idbalasan').value = null;
-    document.getElementById('balasan').innerHTML = null;
-    document.getElementById('formnama').value = null;
-    document.getElementById('hadiran').value = 0;
-    document.getElementById('formpesan').value = null;
-};
-
-const innerCard = (comment) => {
-    let result = '';
-
-    comment.forEach((data) => {
-        result += `
-        <div class="card-body border-start bg-light py-2 ps-2 pe-0 my-2 ms-3 me-0" id="${data.uuid}">
-            <div class="d-flex flex-wrap justify-content-between align-items-center">
-                <p class="text-dark text-truncate m-0 p-0" style="font-size: 0.95rem;">
-                    <strong>${data.nama}</strong>
-                </p>
-                <small class="text-dark m-0 p-0" style="font-size: 0.75rem;">${data.created_at}</small>
-            </div>
-            <hr class="text-dark my-1">
-            <p class="text-dark mt-0 mb-1 mx-0 p-0" style="white-space: pre-line">${data.komentar}</p>
-            <button style="font-size: 0.8rem;" onclick="balasan(this)" data-uuid="${data.uuid}" class="btn btn-sm btn-outline-dark py-0">Balas</button>
-            ${innerCard(data.comment)}
-        </div>`;
-    });
-
-    return result;
-};
-
-const renderCard = (data) => {
-    const DIV = document.createElement('div');
-    DIV.classList.add('mb-3');
-    DIV.innerHTML = `
-    <div class="card-body bg-light shadow p-2 m-0 rounded-3" id="${data.uuid}">
-        <div class="d-flex flex-wrap justify-content-between align-items-center">
-            <p class="text-dark text-truncate m-0 p-0" style="font-size: 0.95rem;">
-                <strong class="me-1">${data.nama}</strong>${data.hadir ? '<i class="fa-solid fa-circle-check text-success"></i>' : '<i class="fa-solid fa-circle-xmark text-danger"></i>'}
-            </p>
-            <small class="text-dark m-0 p-0" style="font-size: 0.75rem;">${data.created_at}</small>
-        </div>
-        <hr class="text-dark my-1">
-        <p class="text-dark mt-0 mb-1 mx-0 p-0" style="white-space: pre-line">${data.komentar}</p>
-        <button style="font-size: 0.8rem;" onclick="balasan(this)" data-uuid="${data.uuid}" class="btn btn-sm btn-outline-dark py-0">Balas</button>
-        ${innerCard(data.comment)}
-    </div>`;
-    return DIV;
-};
-
 
 document.addEventListener('DOMContentLoaded', () => {
     let modal = new bootstrap.Modal('#exampleModal');
-    let name = (new URLSearchParams(window.location.search)).get('to') ?? '';
+    let timeOption = (new URLSearchParams(window.location.search)).get('t') ?? '';
 
-    if (name.length == 0) {
-        document.getElementById('namatamu').remove();
-    } else {
-        name = name.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-        let div = document.createElement('div');
-        div.classList.add('m-2');
-        div.innerHTML = `
-        <p class="mt-0 mb-1 mx-0 p-0 text-light">Kepada Yth Bapak/Ibu/Saudara/i</p>
-        <h2 class="text-light">${name}</h2>
-        `;
-
-        document.getElementById('formnama').value = name;
-        document.getElementById('namatamu').appendChild(div);
+    if (timeOption == 'j') {
+        eventTime = '2023-04-28 13:30:00';
+        document.getElementById('txt-date-title').innerText = 'Jumat, 28 April 2023';
+        document.getElementById('txt-event-name').innerText = 'Akad Nikah';
+        document.getElementById('txt-event-date').innerText = 'Jumat, 28 April 2023';
+        document.getElementById('txt-event-time').innerText = '13.30 WIB - selesai';
+        document.getElementById('btn-map').setAttribute("href", "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Akad%20Nikah%20Zulfi%20dan%20Thoriq&details=Akad%20Nikah%20Zulfi%20dan%20Thoriq%20%0AKoripan%20RT%2002%20RW%2018%2C%20Sindumartani%2C%20Ngemplak%2C%20Sleman%0A13.30%20WIB%20-%20selesai&dates=20230428T133000/20230428T150000&location=https://goo.gl/maps/M2c73YZTjZhzJd5g7");
+    } else if (timeOption == 's9') {
+        eventTime = '2023-04-29 09:00:00';
+        document.getElementById('txt-event-time').innerText = '09.00 - 10.00 WIB';
+        document.getElementById('btn-map').setAttribute("href", "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Resepsi%20Pernikahan%20Zulfi%20dan%20Thoriq&details=Resepsi%20Pernikahan%20Zulfi%20dan%20Thoriq%20%0AKoripan%20RT%2002%20RW%2018%2C%20Sindumartani%2C%20Ngemplak%2C%20Sleman%0A09.00%20-%2010.00%20WIB&dates=20230429T090000/20230429T100000&location=https://goo.gl/maps/M2c73YZTjZhzJd5g7");
+    } else if (timeOption == 's10') {
+        eventTime = '2023-04-29 10:00:00';
+        document.getElementById('txt-event-time').innerText = '10.00 - 11.00 WIB';
+        document.getElementById('btn-map').setAttribute("href", "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Resepsi%20Pernikahan%20Zulfi%20dan%20Thoriq&details=Resepsi%20Pernikahan%20Zulfi%20dan%20Thoriq%20%0AKoripan%20RT%2002%20RW%2018%2C%20Sindumartani%2C%20Ngemplak%2C%20Sleman%0A10.00%20-%2011.00%20WIB&dates=20230429T100000/20230429T110000&location=https://goo.gl/maps/M2c73YZTjZhzJd5g7");
     }
 
     modal.show();
